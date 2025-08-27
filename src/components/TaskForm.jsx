c95474a52d39173a1d34ae9defbceaf789db0c71
ChauNgo1106 <baochaust@gmail.com>
2025-08-27 08:07:56 +0700

    Add simple form validation for adding a new task

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,25 +2,31 @@ import { useState } from "react";
 import "../styles/TaskForm.css";
 function TaskForm({ onAdd }) {
   const [title, setTitle] = useState("");
+  const [error, setError] = useState(false);
+
   const handleOnSubmit = (e) => {
+    setError((e) => false);
     e.preventDefault();
     if (!title.trim()) {
-      alert("Title can not be blank");
+      setError((e) => true);
       return;
     }
     onAdd(title.trim());
     setTitle("");
   };
   return (
-    <form onSubmit={handleOnSubmit}>
-      <input
-        type="text"
-        placeholder="Add a new task ..."
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button type="submit">Add</button>
-    </form>
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="Add a new task ..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {error && <p style={{ color: "red" }}>Please enter your title</p>}
+    </div>
   );
 }
 
